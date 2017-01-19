048a2e64c535cb4d538e2695e08cf66d866fa937
Leo Laksmana <beol@laksmana.com>
2017-01-19 22:21:48 +0000

    Update build steps

--- build.sh
+++ build.sh
@@ -4,12 +4,11 @@ set -ev
 
 BASE_DIR=$(dirname $0)
 
-mkdir -p $BASE_DIR/rpmbuild/BUILD/laksmana-centos-repo
-cp -p $BASE_DIR/{RPM-GPG-KEY-laksmana,laksmana-centos.repo} $BASE_DIR/rpmbuild/BUILD/laksmana-centos-repo/
+cd $BASE_DIR
 
-rpmbuild -bb $BASE_DIR/laksmana-centos-repo.spec
+mkdir -p rpmbuild/SOURCES
+cp -p laksmana-centos.repo RPM-GPG-KEY-laksmana rpmbuild/SOURCES
 
-for file in $(find ${BASE_DIR}/rpmbuild/RPMS -type f -name "*.rpm"); do
-    expect $BASE_DIR/rpm-sign.exp $file
-    rpm --checksig $file
-done
+rpmbuild -bb laksmana-centos-repo.spec
+
+[[ -n "${GPG_PASSPHRASE}" ]] && find ./rpmbuild/RPMS -type f -name "*.rpm" | xargs -I{} sh -c "./rpm-sign.exp {} && rpm --checksig {}"
